35472c23ba51c647726db2241d5d067e2ca3e37c
xpleaf <xpleaf@163.com>
2016-02-10 21:55:57 +0800

    finish pagination

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,26 @@
-from flask import render_template
+from flask import render_template, request, current_app
 from . import main
+from ..models import Article, ArticleType
 
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    page = request.args.get('page', 1, type=int)
+    pagination = Article.query.order_by(Article.create_time.desc()).paginate(
+            page, per_page=current_app.config['ARTICLES_PER_PAGE'],
+            error_out=False)
+    articles = pagination.items
+    return render_template('index.html', ArticleType=ArticleType,
+                           articles=articles, pagination=pagination, endpoint='.index')
+
+
+@main.route('/article-types/<int:id>/')
+def articleTypes(id):
+    page = request.args.get('page', 1, type=int)
+    pagination = ArticleType.query.get_or_404(id).articles.order_by(
+            Article.create_time.desc()).paginate(
+            page, per_page=current_app.config['ARTICLES_PER_PAGE'],
+            error_out=False)
+    articles = pagination.items
+    return render_template('index.html', ArticleType=ArticleType,
+                           articles=articles, pagination=pagination, endpoint='.articleTypes', id=id)
--- config.py
+++ config.py
@@ -3,11 +3,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config():
+    DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_RECORD_QUERIES = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    ARTICLES_PER_PAGE = 10
 
     @staticmethod
     def init_app(app):
