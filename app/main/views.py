7e27f2b3d4c77945c1b61b1a84f6881dcb03e56b
xpleaf <xpleaf@163.com>
2016-03-01 16:26:42 +0800

    finish manage-articleTypes step_6:add aritcleType link and source link in the admin page and home page

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,7 @@ from flask import render_template, request, current_app, redirect,\
     url_for, flash
 from . import main
 from ..models import Article, ArticleType, article_types, Comment, \
-    Follow, User
+    Follow, User, Source
 from .forms import CommentForm
 from .. import db
 
@@ -29,6 +29,19 @@ def articleTypes(id):
     articles = pagination.items
     return render_template('index.html', articles=articles,
                            pagination=pagination, endpoint='.articleTypes',
+                           id=id)
+
+
+@main.route('/article-sources/<int:id>/')
+def article_sources(id):
+    page = request.args.get('page', 1, type=int)
+    pagination = Source.query.get_or_404(id).articles.order_by(
+            Article.create_time.desc()).paginate(
+            page, per_page=current_app.config['ARTICLES_PER_PAGE'],
+            error_out=False)
+    articles = pagination.items
+    return render_template('index.html', articles=articles,
+                           pagination=pagination, endpoint='.article_sources',
                            id=id)
 
 
