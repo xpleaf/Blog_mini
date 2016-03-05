e9019bc9776b897a418bed81a77a662ed56d935f
xpleaf <xpleaf@163.com>
2016-03-05 22:28:15 +0800

    finish add momentjs to locale time

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,13 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.login import LoginManager
 from flask_wtf.csrf import CsrfProtect
+from flask_moment import Moment
 from config import Config
 
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
+moment = Moment()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -21,6 +23,7 @@ def create_app():
 
     db.init_app(app)
     bootstrap.init_app(app)
+    moment.init_app(app)
     login_manager.init_app(app)
 
     from .main import main as main_blueprint
