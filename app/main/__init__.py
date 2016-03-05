4cc52c7a47b5878cfaa782ab29cc252e03ef2a32
xpleaf <xpleaf@163.com>
2016-03-05 22:37:59 +0800

    finish custom errors page

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 main = Blueprint('main', __name__)
 
-from . import views
+from . import views, errors
---
+++ app/main/errors.py
@@ -0,0 +1,20 @@
+from flask import render_template, request, jsonify
+from . import main
+
+
+@main.app_errorhandler(403)
+def forbidden(e):
+
+    return render_template('403.html'), 403
+
+
+@main.app_errorhandler(404)
+def page_not_found(e):
+
+    return render_template('404.html'), 404
+
+
+@main.app_errorhandler(500)
+def internal_server_error(e):
+
+    return render_template('500.html'), 500
