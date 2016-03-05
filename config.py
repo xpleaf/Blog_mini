6cc375f2cc878408e247d0bdd1da7b2eddaf3c1f
xpleaf <xpleaf@163.com>
2016-03-06 00:05:46 +0800

    finish Heroku deploy setting

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config():
-    DEBUG = True
+    # DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     # SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_RECORD_QUERIES = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
     ARTICLES_PER_PAGE = 10
     COMMENTS_PER_PAGE = 6
